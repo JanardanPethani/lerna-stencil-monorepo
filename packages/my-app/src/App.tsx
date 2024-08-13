2d9908259978561393ddd42ee5be570d2eeb15fb
JanardanPethani <jpethani11@gmail.com>
2024-08-13 22:52:15 +0530

    Initialise lerna app

---
+++ packages/my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import { MyComponent } from "react-library";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent first="Your" last="Name" />
+    </div>
+  );
+}
+
+export default App;
